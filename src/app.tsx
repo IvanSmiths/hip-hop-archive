7cc54b316c133f3e77c576f01b2b265df15e289e
Ivan <info@ivansmiths.com>
2025-12-22 12:17:08 +0100

    add drizzle

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,7 @@
+// @refresh reload
 import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
 import { Suspense } from "solid-js";
-import Nav from "~/components/Nav";
 import "./app.css";
 
 export default function App() {
@@ -9,7 +9,8 @@ export default function App() {
     <Router
       root={props => (
         <>
-          <Nav />
+          <a href="/">Index</a>
+          <a href="/about">About</a>
           <Suspense>{props.children}</Suspense>
         </>
       )}
